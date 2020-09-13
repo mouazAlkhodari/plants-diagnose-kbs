ec1a973cb10dee87a4315aadc78939a2175278a1
Mouaz Alkhodari <mouazx19999@gmail.com>
2020-09-13 20:35:02 +0300

    final

--- ask_questions.py
+++ ask_questions.py
@@ -3,7 +3,7 @@ from db import *
 from gui import *
 import codecs
 
-randomChoice = True
+randomChoice = False
 
 ''' asking helper functions '''
 file = codecs.open('samples/test.txt', 'w', 'utf-8')
@@ -16,18 +16,17 @@ def Ask_Choices_Question(question: str, *choices) -> str:
         function that ask question with list of choices to allow the user to choose from
     '''
     # // TODO make it in gui
-    while True:
-        try:
-            answer = input('{}\nchoices:\n{}\nanswer:'.format(
-                question,
-                '\n'.join(
-                    map(lambda choice: f"{choice[0] + 1}) {choice[1]}", enumerate(choices)))
-            ))
-            if answer in choices:
-                return answer
-            raise IOError(f'wrong choice Try Again')
-        except IOError as error:
-            print(f'{B_COLORS.FAIL}ERROR: {error}{B_COLORS.ENDC}')
+    try:
+        answer = input('{}\nchoices:\n{}\nanswer:'.format(
+            question,
+            '\n'.join(
+                map(lambda choice: f"{choice[0] + 1}) {choice[1]}", enumerate(choices)))
+        ))
+        if not answer in choices:
+            answer = choice(choices)
+        raise IOError(f'wrong choice Try Again')
+    except IOError as error:
+        print(f'{B_COLORS.FAIL}ERROR: {error}{B_COLORS.ENDC}')
 
 
 def Ask_Plant_Name() -> str:
